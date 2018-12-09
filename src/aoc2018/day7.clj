ac572fe1542b4bfce6d8fd97b1d7f003d65118f6
Jos van Bakel <josvanbakel@protonmail.com>
2018-12-09 17:32:56 +0100

    Day 7 part 2

--- src/aoc2018/day7.clj
+++ src/aoc2018/day7.clj
@@ -47,20 +47,61 @@ Step F must be finished before step E can begin.")
   (map-values (fn [deps] (filter #(not= x %) deps))
               (dissoc steps x)))
 
+(defn pick-step [steps]
+  (->>
+   steps
+   (filter (comp empty? second))
+   (map first)
+   sort
+   first))
+
 (defn determine-order [steps]
   (loop [result []
          remaining-steps steps]
     (if (empty? remaining-steps)
       (apply str result)
-      (let [choices (filter (comp empty? second) remaining-steps)
-            step (first (sort (map first choices)))]
+      (let [step (pick-step remaining-steps)]
         (if (nil? step)
           (throw (Exception. "Stuck."))
           (recur (conj result step)
                  (complete-step remaining-steps step)))))))
 
+(defn step-runtime [step penalty]
+  (+ (- (int step) (int \A)) 1 penalty))
+
+(defn split-on [f coll]
+  (let [{t true f false} (group-by f coll)]
+    [t f]))
+
+(defn compute-time [steps workers penalty]
+  (loop [time 0
+         remaining-steps steps
+         free-workers workers
+         running-jobs {}]
+    (if (and (empty? remaining-steps) (empty? running-jobs))
+      time
+      (let [step (pick-step remaining-steps)]
+        ;; is a worker available and is there work to be done?
+        (if (and (pos? free-workers) (not (nil? step)))
+          ;; start working on it.
+          (recur time
+                 (dissoc remaining-steps step)
+                 (dec free-workers)
+                 (assoc running-jobs step
+                        (step-runtime step penalty)))
+          ;; decrease time in all jobs.
+          ;; complete all jobs that reached zero.
+          (let [tick (map-values dec running-jobs)
+                [done remaining-jobs] (split-on (comp zero? second) tick)]
+            (recur (inc time)
+                   (reduce complete-step remaining-steps
+                           (map first done))
+                   (+ free-workers (count done))
+                   (into {} remaining-jobs))))))))
+
 (defn main
   "Advent of Code 2018 - Day 7"
   [& args]
   (let [steps (parse (read-data))]
-    (println (determine-order steps))))
+    (println (determine-order steps)
+             (compute-time steps 5 60))))
