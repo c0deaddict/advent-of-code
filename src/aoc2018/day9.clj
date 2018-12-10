9552880473908d23ba680b1958d28d148e564675
Jos van Bakel <josvanbakel@protonmail.com>
2018-12-10 20:03:22 +0100

    Day 9 part 1

--- src/aoc2018/day9.clj
+++ src/aoc2018/day9.clj
@@ -2,7 +2,8 @@
   (require [clojure.java.io :as io]
            [clojure.string :as str]
            [clojure.set]
-           [clojure.pprint :refer [pprint]]))
+           [clojure.pprint :refer [pprint]]
+           [clojure.test :as t]))
 
 ;; https://stackoverflow.com/a/24553906/248948
 (defn drop-nth [n coll]
@@ -20,19 +21,44 @@
   (if (zero? (mod marble 23))
     ;; multiple of 23
     (let [extract (clockwise circle current -7)
-          score (nth circle extract)
-          circle (drop-nth extract circle)
-          current (clockwise circle current -6)]
-      [circle current (+ marble score)])
+          points (nth circle extract)
+          circle (into [] (drop-nth extract circle))
+          current (clockwise circle extract 0)]
+      [circle current (+ marble points)])
     ;; normal flow
     (let [current (clockwise circle current 2)
           circle (insert-at circle current marble)]
       [circle current 0])))
 
-(defn high-score [num-players num-marbles]
-  nil)
+(defn run-game [num-players num-marbles]
+  (loop [circle [0]
+         current 0
+         marble 1
+         scores (zipmap (range num-players) (repeat 0))
+         turns (cycle (range num-players))]
+    (if (> marble num-marbles)
+      scores
+      (let [player (first turns)
+            [circle current points] (do-move circle current marble)]
+        ;; (println player circle current)
+        (recur circle
+               current
+               (inc marble)
+               (update scores player #(+ % points))
+               (rest turns))))))
+
+(defn high-score [scores]
+  (last (sort (map second scores))))
+
+(t/deftest test-high-scores
+  (t/is (= 32 (high-score (run-game 9 25))))
+  (t/is (= 8317 (high-score (run-game 10 1618))))
+  (t/is (= 146373 (high-score (run-game 13 7999))))
+  (t/is (= 2764 (high-score (run-game 17 1104))))
+  (t/is (= 54718 (high-score (run-game 21 6111))))
+  (t/is (= 37305 (high-score (run-game 30 5807)))))
 
 (defn main
   "Advent of Code 2018 - Day 9"
   [& args]
-  (println nil))
+  (println (high-score (run-game 429 70901))))
