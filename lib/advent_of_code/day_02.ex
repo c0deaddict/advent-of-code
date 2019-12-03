bb0bbecf1d76dc2c2c431dcd3c5e931e4592d596
Jos van Bakel <jos@codeaddict.org>
2019-12-03 22:01:16 +0100

    Day 2 part 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -31,14 +31,27 @@ defmodule AdventOfCode.Day02 do
   end
 
   def part1(program) do
+    compute_pair(program, 12, 2)
+  end
+
+  def compute_pair(program, noun, verb) do
     program
-    |> put_elem(1, 12)
-    |> put_elem(2, 2)
+    |> put_elem(1, noun)
+    |> put_elem(2, verb)
     |> run_program(0)
     |> elem(0)
   end
 
+  def cart_product(l1, l2) do
+    for i <- l1, j <- l2, do: {i, j}
+  end
+
   def part2(program) do
-    # find noun (addr 1) and verb (addr 2) which give as result (addr 0) 19690720
+    {noun, verb} = cart_product(0..99, 0..99)
+    |> Enum.find(fn {noun, verb} ->
+      compute_pair(program, noun, verb) == 19690720
+    end)
+
+    100 * noun + verb
   end
 end
