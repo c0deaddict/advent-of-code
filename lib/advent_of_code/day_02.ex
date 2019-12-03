6cec475c1cc84d965a8475f5c3d5d461a72372a7
Jos van Bakel <jos@codeaddict.org>
2019-12-03 13:13:45 +0100

    Day 2 part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Day02 do
-  def part1(args) do
+  def is_halt(program, ip) do
+    elem(program, ip) == 99
   end
 
-  def part2(args) do
+  def run_op(program, ip) do
+    op = elem(program, ip)
+    f = case op do
+      99 -> raise "halt"
+      1 -> &+/2
+      2 -> &*/2
+      _ -> raise "unknown op"
+    end
+
+    lhs_addr = elem(program, ip + 1)
+    rhs_addr = elem(program, ip + 2)
+    res_addr = elem(program, ip + 3)
+
+    lhs = elem(program, lhs_addr)
+    rhs = elem(program, rhs_addr)
+
+    put_elem(program, res_addr, apply(f, [lhs, rhs]))
+  end
+
+  def run_program(program, ip) do
+    if is_halt(program, ip) do
+      program
+    else
+      run_program(run_op(program, ip), ip + 4)
+    end
+  end
+
+  def part1(program) do
+    program
+    |> put_elem(1, 12)
+    |> put_elem(2, 2)
+    |> run_program(0)
+    |> elem(0)
+  end
+
+  def part2(program) do
+    # find noun (addr 1) and verb (addr 2) which give as result (addr 0) 19690720
   end
 end
--- lib/mix/tasks/d02.p2.ex
+++ lib/mix/tasks/d02.p2.ex
@@ -5,7 +5,11 @@ defmodule Mix.Tasks.D02.P2 do
 
   @shortdoc "Day 02 Part 2"
   def run(args) do
-    input = nil
+    input = File.read!("./input/day02.txt")
+    |> String.trim
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
