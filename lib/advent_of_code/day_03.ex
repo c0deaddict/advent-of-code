8212592fe1e10b4e193a92adbad1f4f0046bd64e
Jos van Bakel <jos@codeaddict.org>
2019-12-04 13:27:03 +0100

    Day 3 part 1 and 2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,94 @@
 defmodule AdventOfCode.Day03 do
-  def part1(args) do
+  def parse_move("R" <> digits), do: {:right, String.to_integer(digits)}
+  def parse_move("U" <> digits), do: {:up, String.to_integer(digits)}
+  def parse_move("L" <> digits), do: {:left, String.to_integer(digits)}
+  def parse_move("D" <> digits), do: {:down, String.to_integer(digits)}
+
+  def parse_line(line) do
+    String.split(line, ",")
+    |> Enum.map(&parse_move/1)
   end
 
-  def part2(args) do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def in_dir(:right), do: {1, 0}
+  def in_dir(:up), do: {0, 1}
+  def in_dir(:left), do: {-1, 0}
+  def in_dir(:down), do: {0, -1}
+
+  def trace_line(line) do
+    for({dir, dist} <- line, _ <- 1..dist, do: in_dir(dir))
+    |> Enum.reduce([{0, 0}], fn {dx, dy}, res ->
+      {x, y} = hd(res)
+      pos = {x + dx, y + dy}
+      [pos | res]
+    end)
+    |> Enum.reverse
+  end
+
+  def manhattan_distance({x, y}), do: abs(x) + abs(y)
+
+  def intersect_traces(trace_1, trace_2) do
+    MapSet.intersection(MapSet.new(trace_1), MapSet.new(trace_2))
+  end
+
+  def find_intersections(line_1, line_2) do
+    trace_1 = line_1 |> trace_line
+    trace_2 = line_2 |> trace_line
+
+    intersect_traces(trace_1, trace_2)
+    |> Enum.sort_by(&manhattan_distance/1)
+  end
+
+  def second([_, res | _]), do: res
+
+  def find_closest_intersection(line_1, line_2) do
+    find_intersections(line_1, line_2) |> second
+  end
+
+  def part1(input) do
+    [line_1, line_2] = parse(input)
+
+    find_closest_intersection(line_1, line_2)
+    |> manhattan_distance
+  end
+
+  def trace_with_delay(trace) do
+    trace
+    |> Enum.with_index(0)
+    |> Enum.reduce(%{}, fn {pos, delay}, acc ->
+      Map.update(acc, pos, delay, &min(&1, delay))
+    end)
+  end
+
+  def find_intersections_with_delay(line_1, line_2) do
+    trace_1 = line_1 |> trace_line
+    trace_2 = line_2 |> trace_line
+
+    trace_1_with_delay = trace_with_delay(trace_1)
+    trace_2_with_delay = trace_with_delay(trace_2)
+
+    intersect_traces(trace_1, trace_2)
+    |> Enum.map(fn pos ->
+      delay_1 = trace_1_with_delay[pos]
+      delay_2 = trace_2_with_delay[pos]
+      {pos, delay_1 + delay_2}
+    end)
+    |> Enum.sort_by(&elem(&1, 1))
+  end
+
+  def find_min_signal_delay(line_1, line_2) do
+    find_intersections_with_delay(line_1, line_2)
+    |> second
+    |> elem(1)
+  end
+
+  def part2(input) do
+    [line_1, line_2] = parse(input)
+    find_min_signal_delay(line_1, line_2)
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,19 +3,69 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  test "parse_move" do
+    assert parse_move("R990") == {:right, 990}
+    assert parse_move("U1") == {:up, 1}
+    assert parse_move("L123456") == {:left, 123_456}
+    assert parse_move("D0") == {:down, 0}
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "parse_line" do
+    line = parse_line("R1,L2,U3,D4")
+    assert line == [{:right, 1}, {:left, 2}, {:up, 3}, {:down, 4}]
+  end
 
-    assert result
+  test "part1 example 1" do
+    input = """
+    R8,U5,L5,D3
+    U7,R6,D4,L4
+    """
+
+    assert part1(input) == 6
+  end
+
+  test "part1 example 2" do
+    input = """
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83
+    """
+
+    assert part1(input) == 159
+  end
+
+  test "part1 example 3" do
+    input = """
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+    """
+
+    assert part1(input) == 135
+  end
+
+  test "part2 example 1" do
+    input = """
+    R8,U5,L5,D3
+    U7,R6,D4,L4
+    """
+
+    assert part2(input) == 30
+  end
+
+  test "part2 example 2" do
+    input = """
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83
+    """
+
+    assert part2(input) == 610
+  end
+
+  test "part2 example 3" do
+    input = """
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+    """
+
+    assert part2(input) == 410
   end
 end
