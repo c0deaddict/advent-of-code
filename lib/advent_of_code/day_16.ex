d7b52542a33a996757a7cfbdd228bd6aad461911
Jos van Bakel <jos@codeaddict.org>
2019-12-17 23:21:59 +0100

    Day 16 part 1 + attempt part 2

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,7 +1,76 @@
 defmodule AdventOfCode.Day16 do
-  def part1(args) do
+  def parse(input) do
+    input
+    |> String.trim()
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
   end
 
-  def part2(args) do
+  def last_digit(value), do: abs(rem(value, 10))
+
+  def gcd(a, 0), do: a
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def lcm(a, b), do: div(a * b, gcd(a, b))
+
+  def pattern(out_pos, c) do
+    for(i <- [0, c, 0, -c], _ <- 0..out_pos, do: i)
+    |> Stream.cycle()
+  end
+
+  def phase(signal, times) do
+    signal_len = length(signal)
+
+    0..(signal_len - 1)
+    |> Enum.map(fn out_pos ->
+      cycle =
+        if out_pos == 0 do
+          signal_len
+        else
+          IO.inspect({signal_len, out_pos*4, lcm(signal_len, out_pos*4)})
+          lcm(signal_len, out_pos * 4)
+        end
+
+      const = if cycle < signal_len do
+        div(signal_len, cycle)
+      else
+        1
+      end
+
+      IO.inspect({cycle, const})
+
+      Stream.concat([0], repeat(signal, times))
+      |> Enum.take(cycle + 1)
+      |> Stream.zip(pattern(out_pos, const))
+      |> Enum.reduce(0, fn {a, b}, acc -> acc + a * b end)
+      |> last_digit()
+    end)
+  end
+
+  def run(signal, num_phases, times \\ 1) do
+    Stream.iterate(signal, &phase(&1, times))
+    |> Stream.drop(num_phases)
+    |> Enum.at(0)
+  end
+
+  def part1(input) do
+    input
+    |> parse
+    |> run(100)
+    |> Enum.take(8)
+    |> Enum.join()
+  end
+
+  def repeat(stream, times) do
+    [stream]
+    |> Stream.cycle()
+    |> Stream.take(times)
+    |> Stream.concat()
+  end
+
+  def part2(input) do
+    input
+    |> parse
+    |> run(1, 10000)
   end
 end
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -3,15 +3,31 @@ defmodule AdventOfCode.Day16Test do
 
   import AdventOfCode.Day16
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "part 1 test 1" do
+    input = "12345678"
 
-    assert result
+    result = input
+    |> parse
+    |> run(4)
+
+    assert result = parse("01029498")
   end
 
-  @tag :skip
+  test "part 1 test 2" do
+    input = "80871224585914546619083218645595"
+    assert part1(input) == "24176176"
+  end
+
+  test "part 1 test 3" do
+    input = "19617804207202209144916044189917"
+    assert part1(input) == "73745418"
+  end
+
+  test "part 1 test 4" do
+    input = "69317163492948606335995924319873"
+    assert part1(input) == "52432133"
+  end
+
   test "part2" do
     input = nil
     result = part2(input)
