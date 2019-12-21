2d22b02ec365e4354abc143810ef7c24f026688b
Jos van Bakel <jos@codeaddict.org>
2019-12-21 16:33:39 +0100

    Day 16 part 2

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,4 +1,6 @@
 defmodule AdventOfCode.Day16 do
+  import AdventOfCode.Utils
+
   def parse(input) do
     input
     |> String.trim()
@@ -8,73 +10,34 @@ defmodule AdventOfCode.Day16 do
 
   def last_digit(value), do: abs(rem(value, 10))
 
-  def gcd(a, 0), do: a
-  def gcd(a, b), do: gcd(b, rem(a, b))
+  def pattern_at(out_pos, idx) do
+    out_pos = out_pos + 1
+    cycle = 4 * out_pos
 
-  def lcm(a, b), do: div(a * b, gcd(a, b))
-
-  def pattern(out_pos, c) do
-    for(i <- [0, c, 0, -c], _ <- 0..out_pos, do: i)
-    |> Stream.cycle()
+    case div(rem(idx, cycle), out_pos) do
+      0 -> 0
+      1 -> 1
+      2 -> 0
+      3 -> -1
+    end
   end
 
-  def transpose([]), do: []
-  def transpose([[]|_]), do: []
-  def transpose(a) do
-    [Enum.map(a, &hd/1) | transpose(Enum.map(a, &tl/1))]
-  end
-
-  def phase(signal, len, times) do
-    0..(len * times)-1
-    |> Enum.map(fn out_pos ->
-      pattern(out_pos, 1)
-      |> Stream.drop(1)
-      |> Stream.take(len * times)
-      |> Enum.chunk_every(len)
-      |> transpose()
-      |> Enum.map(&Enum.sum/1)
-      |> Stream.zip(signal)
-      |> Enum.reduce(0, fn {a, b}, acc -> acc + a * b end)
-      |> last_digit()
-    end)
-  end
-
-  def correct_phase(signal, len, times) do
-    0..(len * times)-1
+  def phase(signal, len) do
+    0..(len - 1)
     |> Enum.map(fn out_pos ->
       Stream.concat([0], signal)
-      |> Stream.zip(pattern(out_pos, 1))
-      |> Enum.reduce(0, fn {a, b}, acc -> acc + a * b end)
+      |> Stream.with_index()
+      |> Enum.reduce(0, fn {x, idx}, acc ->
+        acc + x * pattern_at(out_pos, idx)
+      end)
       |> last_digit()
     end)
   end
 
-  def find_cycle(stream) do
-    stream
-    |> Enum.reduce_while(Map.new(), fn i, acc ->
-      case Map.get(acc, i) do
-        nil ->
-          {:cont, Map.put(acc, i, map_size(acc))}
-
-        start ->
-          {:halt, {start, map_size(acc)}}
-      end
-    end)
-  end
-
-  def print(signal, len) do
-    signal
-    |> Enum.chunk_every(len)
-    |> Enum.map(&Enum.join(&1, ""))
-    |> Enum.join(" ")
-    |> IO.puts
-  end
-
-  def run(signal, num_phases, times \\ 1) do
+  def run(signal, num_phases) do
     len = length(signal)
-    signal = repeat(signal, times)
-    Stream.iterate(signal, &phase(&1, len, times))
-    |> Stream.each(&print(&1, len))
+
+    Stream.iterate(signal, &phase(&1, len))
     |> Stream.drop(num_phases)
     |> Enum.at(0)
   end
@@ -87,29 +50,42 @@ defmodule AdventOfCode.Day16 do
     |> Enum.join()
   end
 
-  def repeat(stream, times) do
-    [stream]
-    |> Stream.cycle()
-    |> Stream.take(times)
-    |> Stream.concat()
+  def phase_part2(signal) do
+    signal
+    |> Enum.reverse()
+    |> Enum.reduce([], fn
+      x, [] -> [x]
+      x, acc -> [rem(hd(acc) + x, 10) | acc]
+    end)
   end
 
   def part2(input) do
-    signal = parse("12345678")
-    times = 2
-    len = length(signal)
-    num = 2
+    offset =
+      input
+      |> String.slice(0..6)
+      |> String.to_integer()
 
+    times = 10000
+    signal = parse(input)
+    len = length(signal) * times
+    phases = 100
+    # From 1/2 of total signal we can ignore all but the 1's in the pattern, and
+    # do a running modulo 10.
+    if offset <= div(len, 2) do
+      raise "offset too small for optimization"
+    end
+
+    # We are only interested in out_pos >= offset, so we can drop all numbers
+    # before it. Since the multiplication pattern for those position is all
+    # zeroes.
     signal
-    |> repeat(times)
-    |> Stream.iterate(&correct_phase(&1, len, times))
-    |> Stream.each(&print(&1, len))
-    |> Stream.drop(num)
+    |> repeat(10000)
+    |> Stream.drop(offset)
+    |> Enum.to_list()
+    |> Stream.iterate(&phase_part2/1)
+    |> Stream.drop(phases)
     |> Enum.at(0)
-
-    IO.puts("\nexperimental:\n")
-
-    signal
-    |> run(num, times)
+    |> Enum.take(8)
+    |> Enum.join()
   end
 end
---
+++ lib/advent_of_code/utils.ex
@@ -0,0 +1,33 @@
+defmodule AdventOfCode.Utils do
+  def transpose([]), do: []
+  def transpose([[] | _]), do: []
+
+  def transpose(a) do
+    [Enum.map(a, &hd/1) | transpose(Enum.map(a, &tl/1))]
+  end
+
+  def gcd(a, 0), do: a
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def lcm(a, b), do: div(a * b, gcd(a, b))
+
+  def find_cycle(stream) do
+    stream
+    |> Enum.reduce_while(Map.new(), fn i, acc ->
+      case Map.get(acc, i) do
+        nil ->
+          {:cont, Map.put(acc, i, map_size(acc))}
+
+        start ->
+          {:halt, {start, map_size(acc)}}
+      end
+    end)
+  end
+
+  def repeat(stream, times) do
+    [stream]
+    |> Stream.cycle()
+    |> Stream.take(times)
+    |> Stream.concat()
+  end
+end
