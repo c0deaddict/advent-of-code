59fefaf805908a11aed0b5799fa9c24512718ebc
Jos van Bakel <jos@codeaddict.org>
2020-12-05 10:45:02 +0100

    Day 05

---
+++ days/day_05/src/main.rs
@@ -0,0 +1,59 @@
+use lib::run;
+use std::collections::HashSet;
+
+type Input = Vec<String>;
+
+fn parse_input(input: &str) -> Input {
+    input
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| l.to_owned())
+        .collect()
+}
+
+fn seat_id(boarding_pass: &str) -> usize {
+    let bin_str = boarding_pass
+        .replace(&['F', 'L'][..], "0")
+        .replace(&['B', 'R'][..], "1");
+    usize::from_str_radix(&bin_str, 2).unwrap()
+}
+
+fn part_01(input: &Input) -> usize {
+    input.iter().map(|bp| seat_id(&bp)).max().unwrap()
+}
+
+fn part_02(input: &Input) -> usize {
+    let booked_seats: HashSet<usize> = input.iter().map(|bp| seat_id(&bp)).collect();
+    let all_seats: HashSet<usize> = (0..1024).collect();
+
+    *all_seats
+        .difference(&booked_seats)
+        .find(|seat_id| {
+            booked_seats.contains(&(*seat_id + 1)) && booked_seats.contains(&(*seat_id - 1))
+        })
+        .unwrap()
+}
+
+fn main() {
+    run(
+        1,
+        include_str!("input.txt"),
+        &parse_input,
+        &part_01,
+        &part_02,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_seat_ids() {
+        assert_eq!(seat_id("FBFBBFFRLR"), 357);
+        assert_eq!(seat_id("BFFFBBFRRR"), 567);
+        assert_eq!(seat_id("FFFBBBFRRR"), 119);
+        assert_eq!(seat_id("BBFFBBFRLL"), 820);
+    }
+}
