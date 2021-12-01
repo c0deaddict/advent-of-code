b6d9df0ffa11509b2be7b20e441d2043db14bb7f
Jos van Bakel <jos@codeaddict.org>
2021-12-01 21:39:03 +0100

    2015 day 6 and 7

--- 2015/days/day_06/src/main.rs
+++ 2015/days/day_06/src/main.rs
@@ -1,22 +1,100 @@
-use lib::run;
+use std::collections::HashSet;
 
-type Input = Vec<i32>;
+use lazy_static::lazy_static;
+use lib::run;
+use regex::Regex;
+
+enum Operation {
+    TurnOn,
+    Toggle,
+    TurnOff,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+struct Instruction {
+    op: Operation,
+    from: Position,
+    to: Position,
+}
+
+type Input = Vec<Instruction>;
+
+lazy_static! {
+    static ref INSTRUCTION_RE: Regex =
+        Regex::new(r"^(turn on|toggle|turn off) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
+}
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            let c = INSTRUCTION_RE.captures(l).unwrap();
+            let op = match c.get(1).unwrap().as_str() {
+                "turn on" => Operation::TurnOn,
+                "toggle" => Operation::Toggle,
+                "turn off" => Operation::TurnOff,
+                op => panic!("unknown operation {}", op),
+            };
+            let from = Position {
+                x: c.get(2).unwrap().as_str().parse().unwrap(),
+                y: c.get(3).unwrap().as_str().parse().unwrap(),
+            };
+            let to = Position {
+                x: c.get(4).unwrap().as_str().parse().unwrap(),
+                y: c.get(5).unwrap().as_str().parse().unwrap(),
+            };
+            Instruction { op, from, to }
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    let mut lights = vec![false; 1000 * 1000];
+    for instr in input {
+        for x in instr.from.x..instr.to.x + 1 {
+            for y in instr.from.y..instr.to.y + 1 {
+                let idx = x + y * 1000;
+                match instr.op {
+                    Operation::TurnOn => lights[idx] = true,
+                    Operation::Toggle => lights[idx] = !lights[idx],
+                    Operation::TurnOff => lights[idx] = false,
+                };
+            }
+        }
+    }
+
+    lights.iter().filter(|v| **v).count()
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_02(input: &Input) -> usize {
+    let mut lights = vec![0; 1000 * 1000];
+    for instr in input {
+        for x in instr.from.x..instr.to.x + 1 {
+            for y in instr.from.y..instr.to.y + 1 {
+                let idx = x + y * 1000;
+                match instr.op {
+                    Operation::TurnOn => lights[idx] += 1,
+                    Operation::Toggle => lights[idx] += 2,
+                    Operation::TurnOff => {
+                        lights[idx] = if lights[idx] > 0 {
+                            lights[idx] - 1
+                        } else {
+                            lights[idx]
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    lights.iter().sum()
 }
 
 fn main() {
@@ -28,11 +106,25 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    turn on 0,0 through 999,999
+    toggle 0,0 through 999,0
+    turn off 499,499 through 500,500
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 998996)
+    }
+
+    const EXAMPLE_DATA_2: &'static str = "
+    turn on 0,0 through 0,0
+    toggle 0,0 through 999,999
+    ";
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_2);
+        assert_eq!(part_02(&input), 2000001)
     }
 }
--- 2015/days/day_07/src/main.rs
+++ 2015/days/day_07/src/main.rs
@@ -1,22 +1,130 @@
+use itertools::Itertools;
 use lib::run;
+use std::collections::HashMap;
 
-type Input = Vec<i32>;
+#[derive(Debug, PartialEq, Clone)]
+enum Operation {
+    Const,
+    And,
+    Or,
+    Not,
+    ShiftLeft,
+    ShiftRight,
+}
+
+#[derive(Debug, Clone)]
+struct Gate<'a> {
+    op: Operation,
+    in0: &'a str,
+    in1: Option<&'a str>,
+    out: &'a str,
+}
+
+impl<'a> Gate<'a> {
+    fn new(op: Operation, in0: &'a str, in1: Option<&'a str>, out: &'a str) -> Self {
+        Gate { op, in0, in1, out }
+    }
+
+    fn inputs(&'a self) -> Vec<&'a str> {
+        let mut res = vec![];
+        if self.in0.parse::<u16>().is_err() {
+            res.push(self.in0);
+        }
+        if let Some(in1) = self.in1 {
+            if in1.parse::<u16>().is_err() {
+                res.push(in1);
+            }
+        }
+        res
+    }
+
+    fn eval_input(wires: &HashMap<&'a str, u16>, wire: &'a str) -> u16 {
+        wire.parse().unwrap_or_else(|_| *wires.get(wire).unwrap())
+    }
+
+    fn compute(&'a self, wires: &HashMap<&'a str, u16>) -> u16 {
+        match self.op {
+            Operation::Const => Self::eval_input(wires, self.in0),
+            Operation::And | Operation::Or => {
+                let val0 = Self::eval_input(wires, self.in0);
+                let val1 = Self::eval_input(wires, self.in1.unwrap());
+                if self.op == Operation::And {
+                    val0 & val1
+                } else {
+                    val0 | val1
+                }
+            }
+            Operation::Not => !Self::eval_input(wires, self.in0),
+            Operation::ShiftLeft | Operation::ShiftRight => {
+                let val0 = Self::eval_input(wires, self.in0);
+                let shift: u16 = self.in1.unwrap().parse().unwrap();
+                if self.op == Operation::ShiftLeft {
+                    val0 << shift
+                } else {
+                    val0 >> shift
+                }
+            }
+        }
+    }
+}
+
+type Input<'a> = Vec<Gate<'a>>;
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            let (l, out) = l.splitn(2, " -> ").collect_tuple().unwrap();
+            match l.split_whitespace().collect::<Vec<_>>().as_slice() {
+                [val] => Gate::new(Operation::Const, val, None, out),
+                ["NOT", in0] => Gate::new(Operation::Not, in0, None, out),
+                [in0, "AND", in1] => Gate::new(Operation::And, in0, Some(in1), out),
+                [in0, "OR", in1] => Gate::new(Operation::Or, in0, Some(in1), out),
+                [in0, "LSHIFT", dist] => Gate::new(Operation::ShiftLeft, in0, Some(dist), out),
+                [in0, "RSHIFT", dist] => Gate::new(Operation::ShiftRight, in0, Some(dist), out),
+                _ => panic!("invalid input gate definition: {}", l),
+            }
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn emulate<'a>(input: &'a Input) -> HashMap<&'a str, u16> {
+    let mut wires: HashMap<&str, u16> = HashMap::new();
+    let mut queue: Vec<&Gate> = input.iter().collect();
+    while !queue.is_empty() {
+        // drain_filter would be nicer, but is unstable.
+        let (resolve, new_queue): (Vec<_>, Vec<_>) = queue
+            .into_iter()
+            .partition(|gate| gate.inputs().iter().all(|i| wires.contains_key(i)));
+
+        for gate in resolve {
+            wires.insert(gate.out, gate.compute(&wires));
+        }
+
+        queue = new_queue;
+    }
+
+    wires
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> u16 {
+    *emulate(input).get("a").unwrap()
+}
+
+fn part_02(input: &Input) -> u16 {
+    let a = emulate(input).get("a").unwrap().to_string();
+
+    // Replace gate "$any -> b" with "$a -> b"
+    let mut new_input: Vec<_> = input.iter().cloned().collect();
+    for gate in new_input.iter_mut() {
+        if gate.op == Operation::Const && gate.out == "b" {
+            gate.in0 = &a;
+        }
+    }
+
+    *emulate(&new_input).get("a").unwrap()
 }
 
 fn main() {
@@ -28,11 +136,26 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
-    ";
+    123 -> x
+    456 -> y
+    x AND y -> d
+    x OR y -> e
+    x LSHIFT 2 -> f
+    y RSHIFT 2 -> g
+    NOT x -> h
+    NOT y -> i";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        let wires = emulate(&input);
+        assert_eq!(*wires.get("d").unwrap(), 72);
+        assert_eq!(*wires.get("e").unwrap(), 507);
+        assert_eq!(*wires.get("f").unwrap(), 492);
+        assert_eq!(*wires.get("g").unwrap(), 114);
+        assert_eq!(*wires.get("h").unwrap(), 65412);
+        assert_eq!(*wires.get("i").unwrap(), 65079);
+        assert_eq!(*wires.get("x").unwrap(), 123);
+        assert_eq!(*wires.get("y").unwrap(), 456);
     }
 }
