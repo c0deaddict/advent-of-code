5f1006f89272efcbd7e126cc5b815ce9819cd33a
Jos van Bakel <jos@codeaddict.org>
2021-12-01 07:51:51 +0100

    2021 day 1

--- 2021/days/day_01/src/main.rs
+++ 2021/days/day_01/src/main.rs
@@ -1,22 +1,37 @@
 use lib::run;
 
-type Input = Vec<i32>;
+type Input = Vec<usize>;
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| l.parse::<usize>().unwrap())
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    let mut increases = 0;
+    for i in 1..input.len() {
+        if input[i] > input[i - 1] {
+            increases += 1;
+        }
+    }
+    increases
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_02(input: &Input) -> usize {
+    let mut increases = 0;
+    let mut sum = input[0] + input[1] + input[2];
+    for i in 3..input.len() {
+        let next_sum = sum - input[i-3] + input[i];
+        if next_sum > sum {
+            increases += 1;
+        }
+        sum = next_sum;
+    }
+    increases
 }
 
 fn main() {
@@ -28,11 +43,27 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 7)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 5)
     }
 }
