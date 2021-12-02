2700a37f55c5fbfeda8b7c5356b0c91408a329fc
Jos van Bakel <jos@codeaddict.org>
2021-12-02 07:49:08 +0100

    2021 day 2

--- 2021/days/day_02/src/main.rs
+++ 2021/days/day_02/src/main.rs
@@ -1,22 +1,53 @@
+use itertools::Itertools;
 use lib::run;
 
-type Input = Vec<i32>;
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+type Input<'a> = Vec<(&'a str, i32)>;
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            let (dir, amount) = l.split_whitespace().collect_tuple().unwrap();
+            (dir, amount.parse().unwrap())
+        })
         .collect()
 }
 
 fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+    let mut pos = Position { x: 0, y: 0 };
+    for (dir, amount) in input {
+        match *dir {
+            "forward" => pos.x += amount,
+            "down" => pos.y += amount,
+            "up" => pos.y -= amount,
+            _ => panic!("unknown direction {}", dir),
+        }
+    }
+    pos.x * pos.y
 }
 
 fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+    let mut aim = 0;
+    let mut pos = Position { x: 0, y: 0 };
+    for (dir, amount) in input {
+        match *dir {
+            "forward" => {
+                pos.x += amount;
+                pos.y += aim * amount;
+            }
+            "down" => aim += amount,
+            "up" => aim -= amount,
+            _ => panic!("unknown direction {}", dir),
+        }
+    }
+    pos.x * pos.y
 }
 
 fn main() {
@@ -28,11 +59,23 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 150)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 900)
     }
 }
