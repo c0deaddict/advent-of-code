292ccbeaeb0039b18681d447a9283c5a646174ba
Jos van Bakel <jos@codeaddict.org>
2021-12-02 19:56:54 +0100

    2015 day 8 and 9

--- 2015/days/day_08/src/main.rs
+++ 2015/days/day_08/src/main.rs
@@ -1,22 +1,70 @@
 use lib::run;
+use std::iter::FromIterator;
 
-type Input = Vec<i32>;
+type Input<'a> = Vec<&'a str>;
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn unescape(s: &str) -> String {
+    let mut res = String::new();
+    let mut it = s.chars();
+    if it.next().unwrap() != '"' {
+        panic!("expected string to start with quote");
+    }
+    while let Some(ch) = it.next() {
+        match ch {
+            '"' => {
+                if it.next().is_some() {
+                    panic!("string does not end after quote");
+                }
+                return res;
+            }
+            '\\' => match it.next().unwrap() {
+                'x' => {
+                    let hex = String::from_iter(vec![it.next().unwrap(), it.next().unwrap()]);
+                    let codepoint = u32::from_str_radix(&hex, 16).unwrap();
+                    res.push(char::from_u32(codepoint).unwrap());
+                }
+                ch => res.push(ch),
+            },
+            _ => res.push(ch),
+        }
+    }
+
+    panic!("unexpected end of string");
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    let code: usize = input.iter().map(|l| l.len()).sum();
+    let escaped: usize = input.iter().map(|l| unescape(l).chars().count()).sum();
+    code - escaped
+}
+
+fn escape(s: &str) -> String {
+    let mut res = "\"".to_owned();
+    for ch in s.chars() {
+        match ch {
+            '"' | '\\' => {
+                res.push('\\');
+                res.push(ch);
+            }
+            _ => res.push(ch),
+        }
+    }
+    res.push('"');
+    res
+}
+
+fn part_02(input: &Input) -> usize {
+    let escaped: usize = input.iter().map(|l| escape(l).len()).sum();
+    let original: usize = input.iter().map(|l| l.len()).sum();
+    escaped - original
 }
 
 fn main() {
@@ -27,12 +75,31 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE_DATA_1: &'static str = "
-    ";
+    const EXAMPLE_DATA_1: &'static str = r#"
+    ""
+    "abc"
+    "aaa\"aaa"
+    "\x27"
+    "#;
+
+    #[test]
+    fn test_unescape() {
+        assert_eq!(unescape(r#""abc""#), "abc");
+        assert_eq!(unescape(r#""aaa\"aaa""#), "aaa\"aaa");
+        assert_eq!(unescape(r#""\x27""#), "\x27");
+        assert_eq!(unescape(r#""\\""#), "\\");
+        assert_eq!(unescape(r#""\x27""#).len(), 1);
+    }
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 12)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 19)
     }
 }
--- 2015/days/day_09/src/main.rs
+++ 2015/days/day_09/src/main.rs
@@ -1,22 +1,56 @@
-use lib::run;
+use std::collections::HashSet;
 
-type Input = Vec<i32>;
+use lib::run;
+use petgraph::{graphmap::UnGraphMap, visit::Dfs};
+use regex::Regex;
+
+type Input<'a> = UnGraphMap<&'a str, usize>;
 
 fn parse_input(input: &str) -> Input {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
-        .collect()
+    let re = Regex::new(r"^(.+) to (.+) = (\d+)$").unwrap();
+
+    UnGraphMap::from_edges(
+        input
+            .lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+            .map(|l| {
+                let c = re.captures(l).unwrap();
+                let from = c.get(1).unwrap().as_str();
+                let to = c.get(2).unwrap().as_str();
+                let dist: usize = c.get(3).unwrap().as_str().parse().unwrap();
+                (from, to, dist)
+            }),
+    )
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn all_complete_paths(input: &Input) -> Vec<usize> {
+    let mut distances = vec![];
+    let unvisited: HashSet<_> = input.nodes().collect();
+    let mut queue: Vec<(_, _, _)> = input.nodes().map(|n| (n, 0, unvisited.clone())).collect();
+    while let Some((node, total_dist, mut unvisited)) = queue.pop() {
+        unvisited.remove(node);
+        if unvisited.is_empty() {
+            distances.push(total_dist);
+            continue;
+        }
+
+        for (_, next, dist) in input.edges(node) {
+            if unvisited.contains(next) {
+                queue.push((next, total_dist + dist, unvisited.clone()));
+            }
+        }
+    }
+
+    distances
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    *all_complete_paths(input).iter().min().unwrap()
+}
+
+fn part_02(input: &Input) -> usize {
+    *all_complete_paths(input).iter().max().unwrap()
 }
 
 fn main() {
@@ -28,11 +62,20 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    London to Dublin = 464
+    London to Belfast = 518
+    Dublin to Belfast = 141
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 605)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 982)
     }
 }
