00e12239d3de55eadc3cca62a619193d08c0b70d
Jos van Bakel <jos@codeaddict.org>
2021-12-02 20:18:16 +0100

    2015 day 10

--- 2015/days/day_10/src/main.rs
+++ 2015/days/day_10/src/main.rs
@@ -1,22 +1,32 @@
+use itertools::Itertools;
 use lib::run;
 
-type Input = Vec<i32>;
+type Input<'a> = &'a str;
 
 fn parse_input(input: &str) -> Input {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+    input.trim()
+}
+
+fn look_and_say(s: &str) -> String {
+    s.chars()
+        .group_by(|e| *e)
+        .into_iter()
+        .map(|(key, group)| format!("{}{}", group.count(), key))
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn iterate(s: &str, times: usize) -> usize {
+    (0..times)
+        .fold((*s).to_owned(), |s, _| look_and_say(&s))
+        .len()
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    iterate(input, 40)
+}
+
+fn part_02(input: &Input) -> usize {
+    iterate(input, 50)
 }
 
 fn main() {
@@ -27,12 +37,12 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE_DATA_1: &'static str = "
-    ";
-
     #[test]
-    fn example_part_1() {
-        let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+    fn examples_part_1() {
+        assert_eq!(look_and_say("1"), "11");
+        assert_eq!(look_and_say("11"), "21");
+        assert_eq!(look_and_say("21"), "1211");
+        assert_eq!(look_and_say("1211"), "111221");
+        assert_eq!(look_and_say("111221"), "312211");
     }
 }
