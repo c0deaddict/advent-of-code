5c7860d87044c8bf7d0ebb3bb4085a0ff58b36c6
Jos van Bakel <jos@codeaddict.org>
2021-12-02 21:59:47 +0100

    2015 day 12

--- 2015/days/day_12/src/main.rs
+++ 2015/days/day_12/src/main.rs
@@ -1,22 +1,43 @@
+use json::JsonValue;
 use lib::run;
+use std::convert::TryInto;
 
-type Input = Vec<i32>;
+type Input = JsonValue;
 
 fn parse_input(input: &str) -> Input {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
-        .collect()
+    json::parse(input).unwrap()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn sum_json(value: &JsonValue) -> i64 {
+    match value {
+        JsonValue::Number(_) => value.as_i64().unwrap(),
+        JsonValue::Array(arr) => arr.iter().map(sum_json).sum(),
+        JsonValue::Object(obj) => obj.iter().map(|(_, val)| sum_json(val)).sum(),
+        _ => 0,
+    }
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> i64 {
+    sum_json(input)
+}
+
+fn sum_json_part2(value: &JsonValue) -> i64 {
+    match value {
+        JsonValue::Number(_) => value.as_i64().unwrap(),
+        JsonValue::Array(arr) => arr.iter().map(sum_json_part2).sum(),
+        JsonValue::Object(obj) => {
+            if obj.iter().any(|(_, val)| val.is_string() && val == "red") {
+                0
+            } else {
+                obj.iter().map(|(_, val)| sum_json_part2(val)).sum()
+            }
+        }
+        _ => 0,
+    }
+}
+
+fn part_02(input: &Input) -> i64 {
+    sum_json_part2(input)
 }
 
 fn main() {
@@ -27,12 +48,15 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE_DATA_1: &'static str = "
-    ";
-
     #[test]
     fn example_part_1() {
-        let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        let input = parse_input(r#"{"a":{"b":4},"c":-1}"#);
+        assert_eq!(part_01(&input), 3);
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(r#"{"d":"red","e":[1,2,3,4],"f":5}"#);
+        assert_eq!(part_02(&input), 0)
     }
 }
