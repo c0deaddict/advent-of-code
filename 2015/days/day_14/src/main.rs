90b546de13748ba9a372f80889ddb118c3c2c2e3
Jos van Bakel <jos@codeaddict.org>
2021-12-03 21:27:55 +0100

    2015 day 14 and 15

--- 2015/days/day_14/src/main.rs
+++ 2015/days/day_14/src/main.rs
@@ -1,22 +1,79 @@
 use lib::run;
+use regex::Regex;
 
-type Input = Vec<i32>;
+struct Reindeer<'a> {
+    name: &'a str,
+    speed: usize,
+    fly_time: usize,
+    rest_time: usize,
+}
+
+type Input<'a> = Vec<Reindeer<'a>>;
 
 fn parse_input(input: &str) -> Input {
+    let re = Regex::new(
+        r"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.$",
+    )
+    .unwrap();
+
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            let c = re.captures(l).unwrap();
+            let name = c.get(1).unwrap().as_str();
+            let speed = c.get(2).unwrap().as_str().parse().unwrap();
+            let fly_time = c.get(3).unwrap().as_str().parse().unwrap();
+            let rest_time = c.get(4).unwrap().as_str().parse().unwrap();
+            Reindeer {
+                name,
+                speed,
+                fly_time,
+                rest_time,
+            }
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn compute_distance(r: &Reindeer, time: usize) -> usize {
+    let cycle_time = r.fly_time + r.rest_time;
+    let full_cycles = time / cycle_time;
+    let mut flying = full_cycles * r.fly_time;
+    let current_cycle = time % cycle_time;
+    flying += std::cmp::min(current_cycle, r.fly_time);
+    flying * r.speed
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn max_distance(input: &Input, time: usize) -> usize {
+    input
+        .iter()
+        .map(|r| compute_distance(r, time))
+        .max()
+        .unwrap()
+}
+
+fn part_01(input: &Input) -> usize {
+    max_distance(input, 2503)
+}
+
+fn max_points(input: &Input, time: usize) -> usize {
+    let mut points = vec![0; input.len()];
+    for t in 0..time {
+        let distances: Vec<_> = input.iter().map(|r| compute_distance(r, t + 1)).collect();
+        let max = distances.iter().max().unwrap();
+        for (i, d) in distances.iter().enumerate() {
+            if d == max {
+                points[i] += 1;
+            }
+        }
+    }
+
+    *points.iter().max().unwrap()
+}
+
+fn part_02(input: &Input) -> usize {
+    max_points(input, 2503)
 }
 
 fn main() {
@@ -28,11 +85,19 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+    Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(max_distance(&input, 1000), 1120)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(max_points(&input, 1000), 689)
     }
 }
--- 2015/days/day_15/src/main.rs
+++ 2015/days/day_15/src/main.rs
@@ -1,22 +1,76 @@
 use lib::run;
+use regex::Regex;
 
-type Input = Vec<i32>;
+type Props = Vec<i64>;
+type Input<'a> = Vec<Props>;
 
 fn parse_input(input: &str) -> Input {
+    let re = Regex::new(r"-?\d+").unwrap();
+
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            re.find_iter(l)
+                .filter_map(|d| d.as_str().parse().ok())
+                .collect()
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn add_ingredient(total: &Props, ingredient: &Props, count: usize) -> Props {
+    let mut res = total.clone();
+    for i in 0..5 {
+        res[i] += ingredient[i] * (count as i64);
+    }
+    res
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+// Idea: scan per property (per column; summing over all ingredients). if the
+// sum <= 0 then we can discard the ingredient ratio earlier.
+fn maximize_score(
+    ingredients: &[Props],
+    count: usize,
+    total: &Props,
+    total_calories: Option<i64>,
+) -> i64 {
+    if ingredients.is_empty() {
+        panic!("maximize_score called without ingredients");
+    }
+
+    let current = ingredients[0].clone();
+    let ingredients = &ingredients[1..];
+
+    (1..(101 - count - ingredients.len()))
+        .map(|i| {
+            let total = add_ingredient(total, &current, i);
+            if ingredients.is_empty() {
+                if let Some(total_calories) = total_calories {
+                    if total[4] != total_calories {
+                        return 0;
+                    }
+                }
+
+                total[0..4]
+                    .iter()
+                    .map(|x| if *x < 0 { 0 } else { *x })
+                    .reduce(|a, b| a * b)
+                    .unwrap()
+            } else {
+                maximize_score(ingredients, count + i, &total, total_calories)
+            }
+        })
+        .max()
+        .unwrap()
+}
+
+fn part_01(input: &Input) -> i64 {
+    maximize_score(input, 0, &vec![0; 5], None)
+}
+
+fn part_02(input: &Input) -> i64 {
+    maximize_score(input, 0, &vec![0; 5], Some(500))
 }
 
 fn main() {
@@ -28,11 +82,19 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+    Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 62842880)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 57600000)
     }
 }
