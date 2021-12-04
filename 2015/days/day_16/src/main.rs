7349e83fae9c3a1d558639803ff6da3f975e5e83
Jos van Bakel <jos@codeaddict.org>
2021-12-04 12:03:14 +0100

    2015 day 16

--- 2015/days/day_16/src/main.rs
+++ 2015/days/day_16/src/main.rs
@@ -1,22 +1,79 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
 use lib::run;
+use std::collections::HashMap;
 
-type Input = Vec<i32>;
+type Input<'a> = Vec<(usize, HashMap<&'a str, usize>)>;
 
 fn parse_input(input: &str) -> Input {
     input
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .map(|l| {
+            let (name, properties) = l.splitn(2, ": ").collect_tuple().unwrap();
+            let id = name[4..].parse().unwrap();
+            let map = properties
+                .split(", ")
+                .map(|kv| {
+                    let (key, value) = kv.splitn(2, ": ").collect_tuple().unwrap();
+                    (key, value.parse().unwrap())
+                })
+                .collect();
+            (id, map)
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+lazy_static! {
+    static ref PROPERTIES: HashMap<&'static str, usize> = HashMap::from([
+        ("children", 3),
+        ("cats", 7),
+        ("samoyeds", 2),
+        ("pomeranians", 3),
+        ("akitas", 0),
+        ("vizslas", 0),
+        ("goldfish", 5),
+        ("trees", 3),
+        ("cars", 2),
+        ("perfumes", 1),
+    ]);
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    let mut candidates = input.clone();
+    for (key, value) in PROPERTIES.iter() {
+        candidates.retain(|(_, p)| match p.get(key) {
+            None => true,
+            Some(v) => v == value,
+        });
+    }
+
+    if candidates.len() != 1 {
+        panic!("expected one aunt to be left");
+    }
+
+    candidates[0].0
+}
+
+fn part_02(input: &Input) -> usize {
+    let mut candidates = input.clone();
+    for (key, value) in PROPERTIES.iter() {
+        candidates.retain(|(_, p)| match p.get(key) {
+            None => true,
+            Some(v) => match *key {
+                "cats" | "trees" => v > value,
+                "pomeranians" | "goldfish" => v < value,
+                _ => v == value,
+            },
+        });
+    }
+
+    if candidates.len() != 1 {
+        panic!("expected one aunt to be left");
+    }
+
+    candidates[0].0
 }
 
 fn main() {
@@ -26,13 +83,4 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    const EXAMPLE_DATA_1: &'static str = "
-    ";
-
-    #[test]
-    fn example_part_1() {
-        let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
-    }
 }
