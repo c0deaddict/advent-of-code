79f6d6521413e782618cdaecaf7871b4859fcc47
Jos van Bakel <jos@codeaddict.org>
2021-12-06 06:34:41 +0100

    2021 day 6

--- 2021/days/day_06/src/main.rs
+++ 2021/days/day_06/src/main.rs
@@ -1,22 +1,45 @@
+use itertools::Itertools;
 use lib::run;
+use std::collections::HashMap;
 
-type Input = Vec<i32>;
+type Input = HashMap<usize, usize>;
 
 fn parse_input(input: &str) -> Input {
     input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .sorted()
+        .group_by(|i| *i)
+        .into_iter()
+        .map(|(i, g)| (i, g.count()))
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn simulate(input: &Input, days: usize) -> usize {
+    let mut state = vec![];
+    for i in 0..9 {
+        state.push(*input.get(&i).unwrap_or(&0));
+    }
+
+    for _ in 0..days {
+        let birth = state[0];
+        for j in 1..9 {
+            state[j - 1] = state[j];
+        }
+        state[6] += birth;
+        state[8] = birth;
+    }
+
+    state.iter().sum()
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    simulate(input, 80)
+}
+
+fn part_02(input: &Input) -> usize {
+    simulate(input, 256)
 }
 
 fn main() {
@@ -28,11 +51,18 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    3,4,3,1,2
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 5934)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 26984457539)
     }
 }
