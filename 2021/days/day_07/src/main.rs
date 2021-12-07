4fdb0068dfa7c6cd8403ff34e0349795eac7eb1f
Jos van Bakel <jos@codeaddict.org>
2021-12-07 09:49:53 +0100

    2021 day 7

--- 2021/days/day_07/src/main.rs
+++ 2021/days/day_07/src/main.rs
@@ -1,22 +1,51 @@
+use itertools::Itertools;
 use lib::run;
+use std::cmp;
 
 type Input = Vec<i32>;
 
 fn parse_input(input: &str) -> Input {
     input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+        .trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .sorted()
         .collect()
 }
 
+fn compute_fuel(input: &Input, pos: i32) -> i32 {
+    input.iter().map(|d| (d - pos).abs()).sum()
+}
+
 fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+    let n = input.len();
+    if n % 2 == 1 {
+        compute_fuel(input, input[(n + 1) / 2])
+    } else {
+        cmp::min(
+            compute_fuel(input, input[n / 2]),
+            compute_fuel(input, input[n / 2 + 1]),
+        )
+    }
+}
+
+fn compute_fuel_part2(input: &Input, pos: i32) -> i32 {
+    input
+        .iter()
+        .map(|x| {
+            let d = (x - pos).abs();
+            (d * (d + 1)) / 2
+        })
+        .sum()
 }
 
 fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+    let min = *input.first().unwrap();
+    let max = *input.last().unwrap();
+    (min..=max)
+        .map(|i| compute_fuel_part2(input, i as i32))
+        .min()
+        .unwrap()
 }
 
 fn main() {
@@ -28,11 +57,18 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    16,1,2,0,4,2,7,1,2,14
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 37)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 168)
     }
 }
