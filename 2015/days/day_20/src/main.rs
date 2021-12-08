3164d11e4154018d88e44e9cf532f67eb5bfcf84
Jos van Bakel <jos@codeaddict.org>
2021-12-08 22:18:41 +0100

    2015 day 20

--- 2015/days/day_20/src/main.rs
+++ 2015/days/day_20/src/main.rs
@@ -1,38 +1,57 @@
 use lib::run;
 
-type Input = Vec<i32>;
+type Input = usize;
 
 fn parse_input(input: &str) -> Input {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
-        .collect()
+    input.trim().parse().unwrap()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn part_01(input: &Input) -> usize {
+    (1..)
+        .find(|i| {
+            let mut sum = (i + 1) * 10;
+            let limit = (*i as f64).sqrt() as usize;
+            // Divisors come in pairs.
+            for j in 2..=limit {
+                if i % j == 0 {
+                    sum += 10 * j;
+                    if i / j != j {
+                        sum += 10 * (i / j);
+                    }
+                }
+            }
+            sum >= *input
+        })
+        .unwrap()
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+#[inline(always)]
+fn elf_visits_house(elf: usize, house: usize) -> bool {
+    house / elf <= 50
+}
+
+fn part_02(input: &Input) -> usize {
+    (51..)
+        .find(|i| {
+            let mut sum = i * 11;
+            let limit = (*i as f64).sqrt() as usize;
+            // Divisors come in pairs.
+            for j in 2..=limit {
+                if i % j == 0 {
+                    if elf_visits_house(j, *i) {
+                        sum += 11 * j;
+                    }
+                    let d = i / j;
+                    if d != j && elf_visits_house(d, *i) {
+                        sum += 11 * d;
+                    }
+                }
+            }
+            sum >= *input
+        })
+        .unwrap()
 }
 
 fn main() {
     run(1, include_str!("input.txt"), parse_input, part_01, part_02)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const EXAMPLE_DATA_1: &'static str = "
-    ";
-
-    #[test]
-    fn example_part_1() {
-        let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
-    }
 }
