fce58e866ff30dbd33d8d8bd9d0234ef7363ccd0
Jos van Bakel <jos@codeaddict.org>
2021-12-15 09:49:21 +0100

    2021 day 15

--- 2021/rust/days/day_15/src/main.rs
+++ 2021/rust/days/day_15/src/main.rs
@@ -1,22 +1,83 @@
+use itertools::Itertools;
 use lib::run;
+use petgraph::{algo::dijkstra, graphmap::DiGraphMap};
+use std::collections::HashMap;
 
-type Input = Vec<i32>;
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone, Ord, PartialOrd)]
+struct Position {
+    x: i32,
+    y: i32,
+}
 
-fn parse_input(input: &str) -> Input {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<i32>().unwrap())
+type Input = HashMap<Position, usize>;
+
+fn neighbours(pos: &Position) -> Vec<Position> {
+    [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        .iter()
+        .map(|(x, y)| Position {
+            x: pos.x + x,
+            y: pos.y + y,
+        })
         .collect()
 }
 
-fn part_01(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn parse_input(input: &str) -> Input {
+    input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.trim()
+                .chars()
+                .enumerate()
+                .map(|(x, risk)| {
+                    let x = x as i32;
+                    let y = y as i32;
+                    (Position { x, y }, risk.to_string().parse().unwrap())
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect()
 }
 
-fn part_02(input: &Input) -> i32 {
-    panic!("failed to find answer");
+fn shortest_path(input: &Input) -> usize {
+    let maxx = input.keys().max_by_key(|p| p.x).unwrap().x;
+    let maxy = input.keys().max_by_key(|p| p.y).unwrap().y;
+    let from = Position { x: 0, y: 0 };
+    let to = Position { x: maxx, y: maxy };
+
+    let graph = DiGraphMap::from_edges(input.keys().flat_map(|from| {
+        neighbours(from)
+            .iter()
+            .filter_map(|to| input.get(to).map(|risk| (*from, *to, *risk)))
+            .collect::<Vec<_>>()
+    }));
+
+    *dijkstra(&graph, from, Some(to), |e| *e.2).get(&to).unwrap()
+}
+
+fn part_01(input: &Input) -> usize {
+    shortest_path(&input)
+}
+
+fn extend_map(input: &Input) -> Input {
+    let width = input.keys().max_by_key(|p| p.x).unwrap().x + 1;
+    let height = input.keys().max_by_key(|p| p.y).unwrap().y + 1;
+
+    (0..5)
+        .cartesian_product(0..5)
+        .cartesian_product(input.iter())
+        .map(|((rx, ry), (pos, risk))| {
+            let x = pos.x + rx * width;
+            let y = pos.y + ry * height;
+            let risk = 1 + ((risk + ((rx + ry) as usize) - 1) % 9);
+            (Position { x, y }, risk)
+        })
+        .collect()
+}
+
+fn part_02(input: &Input) -> usize {
+    shortest_path(&extend_map(&input))
 }
 
 fn main() {
@@ -28,11 +89,27 @@ mod tests {
     use super::*;
 
     const EXAMPLE_DATA_1: &'static str = "
+    1163751742
+    1381373672
+    2136511328
+    3694931569
+    7463417111
+    1319128137
+    1359912421
+    3125421639
+    1293138521
+    2311944581
     ";
 
     #[test]
     fn example_part_1() {
         let input = parse_input(EXAMPLE_DATA_1);
-        assert_eq!(part_01(&input), 0)
+        assert_eq!(part_01(&input), 40)
+    }
+
+    #[test]
+    fn example_part_2() {
+        let input = parse_input(EXAMPLE_DATA_1);
+        assert_eq!(part_02(&input), 315)
     }
 }
