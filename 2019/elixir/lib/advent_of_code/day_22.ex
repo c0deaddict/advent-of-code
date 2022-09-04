d372be0be5dded9a70d2b4f3c09ecf2aebf1bcce
Jos van Bakel <jos@codeaddict.org>
2022-09-04 15:38:45 +0200

    2019 day 22

--- 2019/elixir/lib/advent_of_code/day_22.ex
+++ 2019/elixir/lib/advent_of_code/day_22.ex
@@ -1,7 +1,132 @@
 defmodule AdventOfCode.Day22 do
-  def part1(args) do
+  import AdventOfCode.Utils, only: [gcd: 2]
+
+  def parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      case Regex.run(~r/^([[:alpha:] ]+)(?:\s+(-?\d+))?$/, String.trim(line)) do
+        [_, "deal into new stack"] -> :new_stack
+        [_, "cut", num] -> {:cut, String.to_integer(num)}
+        [_, "deal with increment", num] -> {:deal, String.to_integer(num)}
+      end
+    end)
   end
 
-  def part2(args) do
+  defp mod(x, y) when x > 0, do: rem(x, y)
+  defp mod(x, y) when x < 0, do: rem(x, y) + y
+  defp mod(0, _y), do: 0
+
+  def find_index(ops, n, i) do
+    Enum.reduce(ops, i, fn op, j ->
+      case op do
+        :new_stack -> n - 1 - j
+        {:cut, num} -> mod(j - num, n)
+        {:deal, num} -> mod(j * num, n)
+      end
+    end)
+  end
+
+  def shuffle(input, n) do
+    ops = parse(input)
+
+    0..(n - 1)
+    |> Enum.map(&{find_index(ops, n, &1), &1})
+    |> Enum.sort_by(&elem(&1, 0))
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  def part1(input) do
+    n = 10007
+
+    parse(input)
+    |> find_index(n, 2019)
+  end
+
+  # solve c in: b*c mod m = a (mod m)
+  # https://www.geeksforgeeks.org/modular-division/
+  def divmod(a, b, m) do
+    if gcd(b, m) != 1 do
+      raise "inverse does not exist"
+    end
+
+    rem(powmod(b, m - 2, m) * rem(a, m), m)
+  end
+
+  def at_index(ops, n, i) do
+    Enum.reduce(Enum.reverse(ops), i, fn op, j ->
+      case op do
+        :new_stack -> n - 1 - j
+        {:cut, num} -> mod(j + num, n)
+        {:deal, num} -> divmod(j, num, n)
+      end
+    end)
+  end
+
+  def optimize(ops, n) do
+    Enum.reduce(ops, {0, 1}, fn op, {cut, deal} ->
+      {cut, deal} =
+        case op do
+          :new_stack ->
+            # new_stack = [{:deal, -1}, {:cut, 1}]
+            {-cut + 1, -deal}
+
+          {:cut, num} ->
+            {cut + num, deal}
+
+          {:deal, num} ->
+            {cut * num, deal * num}
+        end
+
+      {mod(cut, n), mod(deal, n)}
+    end)
+  end
+
+  # https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+  def powmod(x, y, p) do
+    case rem(x, p) do
+      0 -> 0
+      x -> powmod(x, y, p, 1)
+    end
+  end
+
+  def powmod(_, 0, _, res), do: res
+
+  def powmod(x, y, p, res) do
+    res =
+      if rem(y, 2) == 1 do
+        rem(res * x, p)
+      else
+        res
+      end
+
+    y = div(y, 2)
+    x = rem(x * x, p)
+
+    powmod(x, y, p, res)
+  end
+
+  def repeat(ops, n, times) do
+    {cut, deal} = optimize(ops, n)
+    new_deal = powmod(deal, times, n)
+    new_cut = divmod(cut * mod(new_deal - 1, n), deal - 1, n)
+    {new_cut, new_deal}
+  end
+
+  def summary_to_ops({cut, deal}) do
+    [
+      {:deal, deal},
+      {:cut, cut}
+    ]
+  end
+
+  def part2(input) do
+    n = 119_315_717_514_047
+    times = 101_741_582_076_661
+
+    repeat(parse(input), n, times)
+    |> summary_to_ops
+    |> at_index(n, 2020)
   end
 end
--- 2019/elixir/test/advent_of_code/day_22_test.exs
+++ 2019/elixir/test/advent_of_code/day_22_test.exs
@@ -3,19 +3,179 @@ defmodule AdventOfCode.Day22Test do
 
   import AdventOfCode.Day22
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  test "deal with increment 3" do
+    input = """
+    deal with increment 3
+    """
+    result = shuffle(input, 10)
+    assert result == [0,7,4,1,8,5,2,9,6,3]
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "part1 example1" do
+    input = """
+    deal with increment 7
+    deal into new stack
+    deal into new stack
+    """
+    result = shuffle(input, 10)
+    assert result == [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
+  end
 
-    assert result
+  test "part1 example2" do
+    input = """
+    cut 6
+    deal with increment 7
+    deal into new stack
+    """
+    result = shuffle(input, 10)
+    assert result == [3,0,7,4,1,8,5,2,9,6]
+  end
+
+  test "part1 example3" do
+    input = """
+    deal with increment 7
+    deal with increment 9
+    cut -2
+    """
+    result = shuffle(input, 10)
+    assert result == [6,3,0,7,4,1,8,5,2,9]
+  end
+
+  test "part1 example4" do
+    input = """
+    deal into new stack
+    cut -2
+    deal with increment 7
+    cut 8
+    cut -4
+    deal with increment 7
+    cut 3
+    deal with increment 9
+    deal with increment 3
+    cut -1
+    """
+    result = shuffle(input, 10)
+    assert result == [9,2,5,8,1,4,7,0,3,6]
+  end
+
+  test "deal with increment 3 and 4 is 2" do
+    input1 = """
+    deal with increment 3
+    deal with increment 4
+    """
+    deal1 = shuffle(input1, 10)
+
+    input2 = """
+    deal with increment 2
+    """
+    deal2 = shuffle(input2, 10)
+
+    assert deal1 == deal2
+  end
+
+  test "optimize" do
+    input1 = """
+    deal with increment 1
+    cut 2
+    deal with increment 1
+    cut 3
+    deal with increment 1
+    cut 1
+    """
+    deal1 = shuffle(input1, 10)
+
+    input2 = """
+    deal with increment 1
+    cut 6
+    """
+    deal2 = shuffle(input2, 10)
+
+    assert deal1 == deal2
+
+    assert optimize(parse(input1), 10) == {6, 1}
+  end
+
+  test "optimize 2" do
+    input1 = """
+    cut 2
+    deal with increment 2
+    cut 3
+    deal with increment 2
+    cut 1
+    deal with increment 2
+    """
+    deal1 = shuffle(input1, 10)
+
+    input2 = """
+    deal with increment 8
+    cut 0
+    """
+    deal2 = shuffle(input2, 10)
+
+    assert deal1 == deal2
+
+    assert optimize(parse(input1), 10) == {0, 8}
+  end
+
+  test "replace new stack with deal and cut" do
+    input1 = """
+    deal into new stack
+    """
+    deal1 = shuffle(input1, 10)
+
+    input2 = """
+    deal with increment -1
+    cut 1
+    """
+    deal2 = shuffle(input2, 10)
+
+    assert deal1 == deal2
+  end
+
+  test "optimize with new_stack" do
+    input1 = """
+    deal with increment 1
+    deal into new stack
+    cut 2
+    deal into new stack
+    deal with increment 1
+    cut 3
+    deal with increment 1
+    cut 1
+    deal into new stack
+    """
+    deal1 = shuffle(input1, 10)
+
+    input2 = """
+    deal with increment 9
+    cut 9
+    """
+    deal2 = shuffle(input2, 10)
+
+    assert deal1 == deal2
+
+    assert optimize(parse(input1), 10) == {9, 9}
+  end
+
+  test "repetitions" do
+    input1 = """
+    deal with increment 13
+    cut 53
+    """
+    n = 137
+    IO.puts("")
+    for i <- 1..100 do
+      ops1 = repeat(parse(input1), n, i)
+      ops2 = optimize(parse(String.duplicate(input1, i)), n)
+      assert ops1 == ops2, "for #{i}: #{inspect(ops1)} != #{inspect(ops2)}"
+    end
+  end
+
+  test "powmod" do
+    assert powmod(2, 5, 13) == 6
+  end
+
+  test "divmod" do
+    assert divmod(8, 3, 5) == 1
   end
 end
