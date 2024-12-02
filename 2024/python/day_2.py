06b37e5580f4b5863c121d0f7fb5418790911940
Jos van Bakel <jos@codeaddict.org>
2024-12-02 07:18:34 +0100

    2024 day 2

--- 2024/python/day_2.py
+++ 2024/python/day_2.py
@@ -1,13 +1,40 @@
 def parse_input(input):
-    return input.strip().splitlines()
+    return [[int(d) for d in line.split()] for line in input.strip().splitlines()]
+
+
+def sign(i):
+    return i / abs(i)
+
+
+def is_safe(report):
+    s = None
+    for i in range(len(report) - 1):
+        d = report[i] - report[i + 1]
+        if not (1 <= abs(d) <= 3):
+            return False
+        if i == 0:
+            s = sign(d)
+        elif sign(d) != s:
+            return False
+    return True
 
 
 def part1(input):
-    pass
+    return sum(int(is_safe(report)) for report in input)
+
+
+def problem_dampener(report):
+    yield report
+    for i in range(len(report)):
+        other = list(report)
+        other.pop(i)
+        yield other
 
 
 def part2(input):
-    pass
+    return sum(
+        int(any(is_safe(r) for r in problem_dampener(report))) for report in input
+    )
 
 
 def main():
@@ -23,8 +50,18 @@ if __name__ == "__main__":
 
 
 EXAMPLE_1 = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 """
 
 
 def test_part1():
-    assert part1(parse_input(EXAMPLE_1)) == 0
+    assert part1(parse_input(EXAMPLE_1)) == 2
+
+
+def test_part2():
+    assert part2(parse_input(EXAMPLE_1)) == 4
