345235362d339066b0522e95844566318d484b92
Jos van Bakel <jos@codeaddict.org>
2024-12-03 08:30:06 +0100

    2024 day 3 python

--- 2024/python/day_3.py
+++ 2024/python/day_3.py
@@ -1,13 +1,25 @@
+import re
+
+
 def parse_input(input):
-    return input.strip().splitlines()
+    return input.strip()
 
 
 def part1(input):
-    pass
+    return sum(int(a) * int(b) for a, b in re.findall(r"mul\((\d+),(\d+)\)", input))
 
 
 def part2(input):
-    pass
+    enabled = True
+    result = 0
+    for m in re.finditer(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)", input):
+        if m.group() == "do()":
+            enabled = True
+        elif m.group() == "don't()":
+            enabled = False
+        elif enabled:
+            result += int(m.groups()[0]) * int(m.groups()[1])
+    return result
 
 
 def main():
@@ -23,8 +35,18 @@ if __name__ == "__main__":
 
 
 EXAMPLE_1 = """
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 """
 
 
 def test_part1():
-    assert part1(parse_input(EXAMPLE_1)) == 0
+    assert part1(parse_input(EXAMPLE_1)) == 161
+
+
+EXAMPLE_2 = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+"""
+
+
+def test_part2():
+    assert part2(parse_input(EXAMPLE_2)) == 48
