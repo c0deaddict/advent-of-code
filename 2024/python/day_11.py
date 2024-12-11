ce432c13243efaf7d1df38e7f768bccc9e2ebbdd
Jos van Bakel <jos@codeaddict.org>
2024-12-11 08:13:24 +0100

    2024 day 11 python

--- 2024/python/day_11.py
+++ 2024/python/day_11.py
@@ -1,13 +1,32 @@
+from collections import Counter, defaultdict
+
+
 def parse_input(input):
-    return input.strip().splitlines()
+    return [int(i) for i in input.strip().split()]
 
 
-def part1(input):
-    pass
+def iteration(stones):
+    result = defaultdict(lambda: 0)
+    for i, count in stones.items():
+        if i == 0:
+            result[1] += count
+        elif (l := len(s := str(i))) % 2 == 0:
+            result[int(s[0 : l // 2])] += count
+            result[int(s[l // 2 :])] += count
+        else:
+            result[i * 2024] += count
+    return result
+
+
+def part1(input, n=25):
+    stones = Counter(input)
+    for _ in range(n):
+        stones = iteration(stones)
+    return sum(stones.values())
 
 
 def part2(input):
-    pass
+    return part1(input, 75)
 
 
 def main():
@@ -22,9 +41,11 @@ if __name__ == "__main__":
     main()
 
 
-EXAMPLE_1 = """
-"""
+def test_iteration():
+    input = parse_input("0 1 10 99 999")
+    assert iteration(Counter(input)) == Counter([1, 2024, 1, 0, 9, 9, 2021976])
 
 
 def test_part1():
-    assert part1(parse_input(EXAMPLE_1)) == 0
+    assert part1(parse_input("125 17"), 6) == 22
+    assert part1(parse_input("125 17"), 25) == 55312
