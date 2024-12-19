2f44752b991cefabf323c93cc49bd29aeecadf66
Jos van Bakel <jos@codeaddict.org>
2024-12-19 08:06:18 +0100

    2024 python day 19

--- 2024/python/day_19.py
+++ 2024/python/day_19.py
@@ -1,13 +1,42 @@
+from functools import cache
+
+
 def parse_input(input):
-    return input.strip().splitlines()
+    towels, patterns = input.strip().split("\n\n")
+    return towels.split(", "), patterns.splitlines()
+
+
+def check_pattern(towels, pattern):
+    if not pattern:
+        return True
+
+    for t in towels:
+        if pattern[: len(t)] == t:
+            if check_pattern(towels, pattern[len(t) :]):
+                return True
+    return False
 
 
 def part1(input):
-    pass
+    towels, patterns = input
+    return sum(int(check_pattern(towels, p)) for p in patterns)
+
+
+@cache
+def count_options(towels, pattern):
+    if not pattern:
+        return 1
+
+    count = 0
+    for t in towels:
+        if pattern[: len(t)] == t:
+            count += count_options(towels, pattern[len(t) :])
+    return count
 
 
 def part2(input):
-    pass
+    towels, patterns = input
+    return sum(count_options(tuple(towels), p) for p in patterns)
 
 
 def main():
@@ -23,8 +52,22 @@ if __name__ == "__main__":
 
 
 EXAMPLE_1 = """
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgbw
 """
 
 
 def test_part1():
-    assert part1(parse_input(EXAMPLE_1)) == 0
+    assert part1(parse_input(EXAMPLE_1)) == 6
+
+
+def test_part2():
+    assert part2(parse_input(EXAMPLE_1)) == 16
